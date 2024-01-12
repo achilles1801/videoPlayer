695e6bb6ceb3fff80d6d71b55a58b282fdcebe75
Majd Khawaldeh <majd@gatech.edu>
2024-01-12 13:58:23 -0500

    cloudfront implem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,63 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      uploadVideo(file);
+    }
+  };
+
+  const uploadVideo = (file) => {
+    fetch('http://localhost:8080/presign?filename=' + file.name)
+      .then(response => response.json())
+      .then(data => {
+        const { url } = data;
+        return fetch(url, {
+          method: 'PUT',
+          body: file,
+        });
+      })
+      .then(response => {
+        if (response.ok) {
+          console.log('Video uploaded successfully');
+          fetchVideos(); // Fetch the updated list of videos
+        } else {
+          console.error('Upload failed', response.statusText);
+        }
+      })
+      .catch(error => {
+        console.error('Error uploading video:', error);
+        console.error('Error details:', error.message);
+      });
+  };
+  
+  const fetchVideos = () => {
+    fetch('http://localhost:8080/list')
+      .then(response => response.json())
+      .then(data => setVideos(data))
+      .catch(error => console.error('Error fetching videos:', error));
+  };
+  
+  useEffect(() => {
+    fetchVideos();
+  }, []);
+  
+
   return (
     <div className="App">
       <h2>Video Streaming Site</h2>
-      <video width="700px" height="400px" controls > 
-      <source src="https://d2ufs6yhgycudn.cloudfront.net/uploads/20231231225832-1e94e06e-a501-482a-a4d9-178f00c9c17a-834331ab4211531d0e81f82841f82555_seamless_loop.mp4" type = "video/mp4"/>
-      </video>
-
+      <div>
+      <input type="file" accept="video/*" onChange={handleFileChange} />
+    </div>
+      {videos.map((videoUrl, index) => (
+  <video key={index} width="700px" height="400px" controls>
+    <source src={videoUrl} type="video/mp4" />
+  </video>
+))}
     </div>
   );
 }
--- main.go
+++ main.go
@@ -3,26 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
-    "github.com/gin-contrib/cors"
 )
 
 func main() {
     router := gin.Default() // entry point for our application
 
+    config := cors.DefaultConfig()
+    config.AllowOrigins = []string{"http://localhost:3000"} // Replace with your frontend URL
+    router.Use(cors.New(config))
+
     router.GET("/presign", handlePresign) // when a GET request is made to /presign, call handlePresign
     router.GET("/list", handleListBucket)  // when a GET request is made to /list, call handleListBucket
 
-    config := cors.DefaultConfig()
-    config.AllowOrigins = []string{"http://localhost:8080"} // Replace with your frontend URL
-    router.Use(cors.New(config))
-    
     // Serve the main app when visiting the root path "/"
     router.GET("/", func(c *gin.Context) {
         c.File("./frontend/index.html")
@@ -47,7 +49,23 @@ func handleListBucket(c *gin.Context) {
         return
     }
 
-    c.JSON(http.StatusOK, result)
+    // Create a slice to hold the URLs
+    urls := []string{}
+
+    // CloudFront distribution URL
+    cloudFrontURL := "https://d2ufs6yhgycudn.cloudfront.net/"
+
+    // Iterate over the objects in the bucket
+    for _, object := range result.Contents {
+        // Check if the object is a .mp4 video
+        if strings.HasSuffix(*object.Key, ".mp4") {
+            // Generate the URL for each object using CloudFront URL
+            url := cloudFrontURL + *object.Key
+            urls = append(urls, url)
+        }
+    }
+
+    c.JSON(http.StatusOK, urls)
 }
 
 func handlePresign(c *gin.Context) {
@@ -77,37 +95,16 @@ func handlePresign(c *gin.Context) {
         c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate pre-signed URL"})
         return
     }
+    log.Println("pre-signed url:",urlStr)
 
     c.JSON(http.StatusOK, gin.H{"url": urlStr})
 }
 
-
-func handleFileUpload(c *gin.Context) {
-    sess := createAWSSession()
-    svc := s3.New(sess)
-
-    // Define bucket and object name (key) for the upload
-    bucket := "majdks-video-player-bucket"
-	uniqueID := uuid.New().String()
-    timestamp := time.Now().Format("20060102150405") // Format: YYYYMMDDHHMMSS
-    key := "uploads/" + timestamp + "-" + uniqueID + ".mp4"
-
-    // Generate pre-signed URL
-    req, _ := svc.PutObjectRequest(&s3.PutObjectInput{
-        Bucket: &bucket,
-        Key:    &key,
-    })
-    urlStr, err := req.Presign(15 * time.Minute) // URL expires in 15 minutes
-    if err != nil {
-        // Handle error
-    }
-
-    c.JSON(http.StatusOK, gin.H{"url": urlStr})
-}
 
 func createAWSSession() *session.Session {
     sess, err := session.NewSession(&aws.Config{
         Region: aws.String("us-east-2"), // Set your AWS region
+        Credentials: credentials.NewStaticCredentials("AKIAYP5CN6RKM73QN2QR", "pIP23PYfDOhx6qgZ8muTi53QDBuv6DKsKfOZd8/W", ""),
     })
     if err != nil {
         log.Fatalf("failed to create session: %s", err.Error())
